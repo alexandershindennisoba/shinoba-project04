065aed2fde22dee1eadb0e549ab972fa3371369a
Alexander <alexander.shin87@gmail.com>
2019-02-23 18:55:48 -0500

    filtered art list with images

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # shinoba-project04
 Our Very First Project
-BRRAPP BRRAPP
-
-Added Test Branch
--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,12 @@ const myWeatherApp = {};
 myWeatherApp.apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
 myWeatherApp.apiKey = '53676921d77f931b9699b38ab357d31e';
   
+const myArtApp = {};
+myArtApp.apiUrl = 'https://www.rijksmuseum.nl/api/en/collection/';
+myArtApp.apiKey = 'ge9zS0UR';
 
 // Weather Promise
 myWeatherApp.getUserCity = (city) => {
-  // console.log(`the city is ${city}`);
   myWeatherApp.getWeather = $.ajax({
     url: myWeatherApp.apiUrl,
     method: 'GET',
@@ -20,36 +22,45 @@ myWeatherApp.getUserCity = (city) => {
       units: 'metric'
     }
   }).then((res) => {
-    console.log(res);
+    // Gives the result of the city weather condition and input that condition into getArt
+    myArtApp.getArt(res.weather[0].main)
   })
   .fail((error) => {
+    // console.log(error);
   })
 };
 
 // Art Promise
-// const getArt = (weatherCondition) => {
-  // myArtApp.getArt = (query) => {
-  // console.log(`the art is of ${query}`);
-  // console.log(query);
-  // myArtApp.getArt = $.ajax({
-  //   url: myArtApp.apiUrl,
-  //   method: 'GET',
-  //   dataType: 'json',
-  //   data: {
-  //     key: myArtApp.apiKey,
-  //     format: 'json',
-  //     q: query
-  //   }
-  // }).then((res) => {
-    // console.log(res.artObjects[0].webImage.url);
-//     const artList = res.artObjects.filter((value) => {
-//       return value.webImage.url != ''
-//     });
-//     console.log(artList);
-//   }).fail((error) => {
-//     console.log(error);
-//   })
-// }
+  myArtApp.getArt = (query) => {
+  myArtApp.getArt = $.ajax({
+    url: myArtApp.apiUrl,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      key: myArtApp.apiKey,
+      format: 'json',
+      q: query
+    }
+  }).then((res) => {
+    // res is result of the query weather condition. gives back an object with array of artObjects
+    // console.log(res);
+    const artList = res.artObjects.filter((value) => {
+      // only returns images with value of true
+      return value.hasImage != ''
+    });
+    console.log(artList);
+    // take array of returned images and run a for loop
+    for(let i = 0; i < artList.length; i++) {
+      // returns indexes of array
+      // choose random array index
+      const randomArtImage = Math.floor(Math.random() * artList.length);
+      console.log(artList[randomArtImage].webImage.url);
+
+    }
+  }).fail((error) => {
+    console.log(error);
+  })
+}
 
 // const imageList = res.artObjects[i].map()
 // imageList = [0, 1, ]
