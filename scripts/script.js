32091857b030de08e04a79174a6f228b53a35c6e
Dennis <oba.dennis@gmail.com>
2019-02-23 23:01:14 -0500

    Minor Edits

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
-
-// Create myApp namespace to hold all properties
+//Weather Api 
 const myWeatherApp = {};
 myWeatherApp.apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
 myWeatherApp.apiKey = '53676921d77f931b9699b38ab357d31e';
-  
+
+shinobaApp = {};
+shinobaApp.weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather';
+
 
 // Weather Promise
 myWeatherApp.getUserCity = (city) => {
-  // console.log(`the city is ${city}`);
   myWeatherApp.getWeather = $.ajax({
     url: myWeatherApp.apiUrl,
     method: 'GET',
@@ -27,53 +28,48 @@ myWeatherApp.getUserCity = (city) => {
 };
 
 // Art Promise
-// const getArt = (weatherCondition) => {
-  // myArtApp.getArt = (query) => {
-  // console.log(`the art is of ${query}`);
-  // console.log(query);
-  // myArtApp.getArt = $.ajax({
-  //   url: myArtApp.apiUrl,
-  //   method: 'GET',
-  //   dataType: 'json',
-  //   data: {
-  //     key: myArtApp.apiKey,
-  //     format: 'json',
-  //     q: query
-  //   }
-  // }).then((res) => {
-    // console.log(res.artObjects[0].webImage.url);
-//     const artList = res.artObjects.filter((value) => {
-//       return value.webImage.url != ''
-//     });
-//     console.log(artList);
-//   }).fail((error) => {
-//     console.log(error);
-//   })
-// }
+const getArt = (weatherCondition) => {
+  myArtApp.getArt = (query) => {
+  	console.log(`the art is of ${query}`);
+  	console.log(query);
+  	myArtApp.getArt = $.ajax({
+			url: myArtApp.apiUrl,
+			method: 'GET',
+			dataType: 'json',
+			data: {
+				key: myArtApp.apiKey,
+				format: 'json',
+				q: query
+			}
+		}).then((res) => {
+			console.log(res.artObjects[0].webImage.url);
+			const artList = res.artObjects.filter((value) => {
+				return value.webImage.url != ''
+			});
+			console.log(artList);
+		}).fail((error) => {
+			console.log(error);
+		})
+	}
+}
 
-// const imageList = res.artObjects[i].map()
-// imageList = [0, 1, ]
+	myArtApp.displayArt = (pieces) => {
+		console.log(pieces);
+		if (pieces.hasImage === true){
+		}
+	}
 
-
-
-// myArtApp.displayArt = (pieces) => {
-//   console.log(pieces);
-//   if (pieces.hasImage === true){
-//   }
-// }
-
-
-  // Start myWeatherApp
+// Start myWeatherApp
   myWeatherApp.init = () => {
     myWeatherApp.getUserCity('toronto');
     // myArtApp.getArt();
   }
   
-  // DOC READY
-  $(function (){
-    myWeatherApp.init();
-    // console.log('Ready');
-  });
+// DOC READY
+	$(function (){
+		myWeatherApp.init();
+		// console.log('Ready');
+});
 
 // PSEUDO CODE
 // CONNECT MUSEUM API
@@ -81,3 +77,6 @@ myWeatherApp.getUserCity = (city) => {
 // THAT IS THE VALUE THAT WILL BE 
 // run reijk museum api into above
 // cheeck classical music, sort according to weather
+
+// const imageList = res.artObjects[i].map()
+// imageList = [0, 1,]
