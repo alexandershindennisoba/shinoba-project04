c714ce3b6816fa7f67a9217db8d6ed94dfee46ce
Dennis <oba.dennis@gmail.com>
2019-02-23 23:12:40 -0500

    Attempting to Get Alex's Code

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,11 @@ const myWeatherApp = {};
 myWeatherApp.apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
 myWeatherApp.apiKey = '53676921d77f931b9699b38ab357d31e';
 
-shinobaApp = {};
-shinobaApp.weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather';
-
+  
+const myArtApp = {};
+myArtApp.apiUrl = 'https://www.rijksmuseum.nl/api/en/collection/';
+myArtApp.apiKey = 'ge9zS0UR';
+>>>>>>> 98145eb5efa1c83f6a8c1899d2d7a2035a6d1017
 
 // Weather Promise
 myWeatherApp.getUserCity = (city) => {
@@ -21,37 +23,57 @@ myWeatherApp.getUserCity = (city) => {
       units: 'metric'
     }
   }).then((res) => {
-    console.log(res);
+    // Gives the result of the city weather condition and input that condition into getArt
+    myArtApp.getArt(res.weather[0].main)
   })
   .fail((error) => {
+    // console.log(error);
   })
 };
 
 // Art Promise
-const getArt = (weatherCondition) => {
+
   myArtApp.getArt = (query) => {
-  	console.log(`the art is of ${query}`);
-  	console.log(query);
-  	myArtApp.getArt = $.ajax({
-			url: myArtApp.apiUrl,
-			method: 'GET',
-			dataType: 'json',
-			data: {
-				key: myArtApp.apiKey,
-				format: 'json',
-				q: query
-			}
-		}).then((res) => {
-			console.log(res.artObjects[0].webImage.url);
-			const artList = res.artObjects.filter((value) => {
-				return value.webImage.url != ''
-			});
-			console.log(artList);
-		}).fail((error) => {
-			console.log(error);
-		})
-	}
+  myArtApp.getArt = $.ajax({
+    url: myArtApp.apiUrl,
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      key: myArtApp.apiKey,
+      format: 'json',
+      q: query
+    }
+  }).then((res) => {
+    // res is result of the query weather condition. gives back an object with array of artObjects
+    // console.log(res);
+    const artList = res.artObjects.filter((piece) => {
+      // only returns images with value of true
+      return piece.hasImage != ''
+    });
+    console.log(artList);
+    // take array of returned images and run a for loop
+    // for(let i = 0; i < artList.length; i++) {
+      // returns indexes of array
+      // choose random array index
+    
+    // map through the array and return array of image urls
+    const filteredArtList = artList.map((piece) => {
+      return piece.webImage.url
+    })
+    console.log(filteredArtList);
+    
+    const randomArtImage = Math.floor(Math.random() * filteredArtList.length);
+    console.log(filteredArtList[randomArtImage]);
+
+    // }
+  }).fail((error) => {
+    console.log(error);
+  })
 }
+
+// const imageList = res.artObjects[i].map()
+// imageList = [0, 1, ]
+>>>>>>> 98145eb5efa1c83f6a8c1899d2d7a2035a6d1017
 
 	myArtApp.displayArt = (pieces) => {
 		console.log(pieces);
