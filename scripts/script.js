faef31155d6ae509d1334e5a91045deb3edd3f30
Dennis <oba.dennis@gmail.com>
2019-02-25 08:13:38 -0500

    Added Music lol

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,32 @@ shinobaApp.weatherApiKey = '53676921d77f931b9699b38ab357d31e';
 shinobaApp.artApiUrl = 'https://www.rijksmuseum.nl/api/en/collection/';
 shinobaApp.artApiKey = 'ge9zS0UR';
 
-const audio1 = $('audio-1')[0];
-const audio2 = $('audio-2')[0];
-const audio3 = $('audio-3')[0];
-const audio4 = $('audio-4')[0];
+const audio1 = $('#audio-1')[0];
+const audio2 = $('#audio-2')[0];
+const audio3 = $('#audio-3')[0];
+const audio4 = $('#audio-4')[0];
 
+const playMusic = () => {
+	const pause = {}
+	if (checkPause.status === false) {
+		checkPause.status = true;
+		audio4.pause();
+		// audio4.currentTime = 0;
+	} else {
+		checkPause.status = false
+		audio4.play();
+	}
+}
 
+const checkPause = {
+	status: false
+}
+$('button').on('click', function (e) {
+	e.preventDefault();
+	playMusic();
+})
+
+// -------------
 
 //Get Weather Temperature
   shinobaApp.getWeather = (temp) => {
@@ -27,6 +47,7 @@ const audio4 = $('audio-4')[0];
 				}
 			}).then((res) => {
 				//Populate the weather object
+				weather.status = true
 				weather.city = res.name;
 				weather.temp = Math.floor(res.main.temp);
 				weather.condition = res.weather[0].main;
@@ -72,6 +93,7 @@ const audio4 = $('audio-4')[0];
 		//Random Image Picker
 		const randomArtImage = Math.floor(Math.random() * filteredArtList.length);
 		//Populates the selectedArt object
+		selectedArt.status = true;
 		selectedArt.title = filteredArtList[randomArtImage].title;
 		selectedArt.artist = filteredArtList[randomArtImage].principalOrFirstMaker;
 		selectedArt.url = filteredArtList[randomArtImage].webImage.url;
@@ -83,10 +105,9 @@ const audio4 = $('audio-4')[0];
 //Syncs all the information to trigger the DOM at the same time
 	shinobaApp.sync = ["temp", "artPiece"];
 	shinobaApp.checkSync = () => {
-		console.log(shinobaApp.sync)
 		const [weather] = shinobaApp.sync;
 		const [,art] =  shinobaApp.sync;
-		if (shinobaApp.sync[0] != '' && shinobaApp.sync[1] != '') {
+		if (shinobaApp.sync[0].status === true && shinobaApp.sync[1].status === true) {
 			//Triggers function at the same time
 			shinobaApp.displayArt(art.url);
 			shinobaApp.displayTitle(art.title);
@@ -94,71 +115,43 @@ const audio4 = $('audio-4')[0];
 			shinobaApp.displayTemp(weather.temp);
 			shinobaApp.displayCity(weather.city);
 			shinobaApp.displayCondition(weather.condition);
-			$('.info-container').fadeIn();
-			$('.info-container').addClass('animated slideInLeft delay-.5s fadeIn delay-.5s');
 			setTimeout(function () {
-				$('.painting').addClass('painting-left')
-			}, 400);
+				$('button').addClass('button-opacity');
+			}, 1000)
 		}
 }
-
-// AUDIO PLAYER
-// audioPlayer();
-function audioPlayer() {
-  let currentSong = 0;
-  $('.audioPlayer')[0].src = $('.playlist li a')[0];
-  $('.audioPlayer')[0].play();
-  $('.audioPlayer')[0].volume = .25;
-  $('.playlist li a').on('click', (e) => {
-    e.preventDefault();
-    $('.audioPlayer')[0].src = this;
-    $('.audioPlayer')[0].play();
-    $('.playlist li').removeClass('current-song');
-  });
-
-  $('.audioPlayer')[0].addEventListener('ended', () => {
-    currentSong++;
-    if (currentSong === $('.playlist li a').length)
-      currentSong = 0;
-    $('.playlist li').removeClass('current-song');
-    $('.playlist li:eq(' + currentSong + ')').addClass('current-song');
-    $('.audioPlayer')[0].src = $('.playlist li a')[currentSong].href;
-    $('.audioPlayer')[0].play();
-  });
-}
-
 
 //DOM Manipulation Area
 	// Display Art and Info
 	shinobaApp.displayArt = (art) => {
-		console.log(art);
-
+		$('.info-container').fadeIn(1000);
+		$('.info-container').addClass('animated slideInLeft slow delay-.1s fadeIn delay-.1s');
 		$('.painting').css('background-image', `url("${art}")`);
-	}
+		$('.painting').addClass('painting-left');
+		setTimeout(function () {
+				$('.painting').fadeIn(1200);
+			}, 500);
+		}
+		
 	shinobaApp.displayTitle = (title) => {
-		console.log(title);
+		$('.info-painting').fadeIn(1500);
 		$('.info-painting h2').text(title);
 	}
 	shinobaApp.displayArtist = (artist) => {
-		console.log(artist);
 		$('.info-painting h3').text(artist);
 	}
 	//Display Weather and Info
 	shinobaApp.displayTemp = (temp) => {
-		console.log(temp);
 		$('.temp').text(temp);
-		$('.temp').fadeIn();
-		$('.temp-bg').fadeIn();
+		$('.temp').fadeIn(1500);
+		$('.temp-bg').fadeIn(1200);
 	}
 	shinobaApp.displayDate = (date) => {
-		console.log(date);
 	}
 	shinobaApp.displayCity = (city) => {
-		console.log(city);
 		$('.city').text(`Location: ${city}`);
 	}
 	shinobaApp.displayCondition = (condition) => {
-		console.log(condition);
 		$('.condition').text(`Condition: ${condition}`);
 	}
 
@@ -168,11 +161,17 @@ function audioPlayer() {
 		$('.info-weather').toggle();
 	})
 
+
 //Get's the user's input here
 	shinobaApp.getUserCity = (userCity) => {
 		$('form').on('submit', function(e) {
 			e.preventDefault();
+			audio4.play();
 			
+			$('.temp').fadeOut();
+			$('.info-painting').fadeOut();
+			$('.info-weather').fadeOut();
+			$('.painting').fadeOut(300);
 			$('.con').addClass('animated fadeIn delay-1s');
 			$('.con').addClass('con-class');
 			$('.search-bar').addClass('search-bottom');
@@ -184,33 +183,12 @@ function audioPlayer() {
 		})
 	}
 
-
 //Triggers the first function
 	shinobaApp.init = () => {
 		shinobaApp.getUserCity();
 	}
 
-
 // DOC READY
 	$(function () {
 		shinobaApp.init();
 	});
-
-
-
-
-  
-
-
-
-
-
-// PSEUDO CODE
-// CONNECT MUSEUM API
-// GET 'WEATHER' OBJECT THAT HAS PROPERTY 'MAIN' 
-// THAT IS THE VALUE THAT WILL BE 
-// run reijk museum api into above
-// cheeck classical music, sort according to weather
-
-// const imageList = res.artObjects[i].map()
-// imageList = [0, 1,]
